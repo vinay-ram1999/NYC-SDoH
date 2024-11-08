73f9874d98836458359926be6a38fc6627af9459
Vinay Ram Gazula <vinayramgazula@gmail.com>
2024-11-08 13:58:23 -0500

    added schema to notebook;

--- sql/reload_nyc_crime.sql
+++ sql/reload_nyc_crime.sql
@@ -3,6 +3,7 @@ SELECT * FROM nyc_crime;
 SELECT YEAR(report_date) AS r_dt, COUNT(*) FROM nyc_crime GROUP BY r_dt; -- This table is the bottle neck (less data available)
 
 -- Default nyc_crime table columns
+/*
 CREATE TABLE nyc_crime_2024 (
 	cmplnt_num text,
 	cmplnt_fr_dt datetime DEFAULT NULL,
@@ -41,6 +42,50 @@ CREATE TABLE nyc_crime_2024 (
 	vic_sex text,
   PRIMARY KEY (cmplnt_num)
 );
+*/
 
+DESCRIBE nyc_crime;
+
+/*
+CREATE TABLE `nyc_crime` (
+  `id` int NOT NULL,
+  `CMPLNT_NUM` text,
+  `ADDR_PCT_CD` double DEFAULT NULL,
+  `BORO_NM` text,
+  `CRM_ATPT_CPTD_CD` text,
+  `HADEVELOPT` text,
+  `HOUSING_PSA` text,
+  `JURISDICTION_CODE` int DEFAULT NULL,
+  `JURIS_DESC` text,
+  `KY_CD` int DEFAULT NULL,
+  `LAW_CAT_CD` text,
+  `LOC_OF_OCCUR_DESC` text,
+  `OFNS_DESC` text,
+  `PARKS_NM` text,
+  `PATROL_BORO` text,
+  `PD_CD` double DEFAULT NULL,
+  `PD_DESC` text,
+  `PREM_TYP_DESC` text,
+  `report_date` datetime DEFAULT NULL,
+  `STATION_NAME` text,
+  `SUSP_AGE_GROUP` text,
+  `SUSP_RACE` text,
+  `SUSP_SEX` text,
+  `TRANSIT_DISTRICT` text,
+  `VIC_AGE_GROUP` text,
+  `VIC_RACE` text,
+  `VIC_SEX` text,
+  `X_COORD_CD` int DEFAULT NULL,
+  `Y_COORD_CD` int DEFAULT NULL,
+  `Latitude` double DEFAULT NULL,
+  `Longitude` double DEFAULT NULL,
+  `Lat_Lon` text,
+  `New Georeferenced Column` text,
+  `zipcode` varchar(10) DEFAULT NULL,
+  `cmplnt_from_date` datetime DEFAULT NULL,
+  `cmplnt_to_date` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`)
+);
+*/
 
 
